18656743738ce1aebe263e053a1abc9603b95d15
Leon Kan <57278679+leonkansh@users.noreply.github.com>
2021-05-03 19:01:55 -0700

    following Jacob's demo

--- day-09/day-09/js/index.js
+++ day-09/day-09/js/index.js
@@ -2,10 +2,10 @@
 'use strict';
 
 // Constants / global variables
-const height = 255;
-const width = 255;
-const fill = '#d3d3d3';
-const namespace = 'http://www.w3.org/2000/svg';
+const HEIGHT = 255;
+const WIDTH = 255;
+const FILL = '#d3d3d3';
+const NS = 'http://www.w3.org/2000/svg'; // NS -> namespace
 
 // State of our application
 let state = {
@@ -13,43 +13,63 @@ let state = {
 };
 
 // Create a `p` element DOM
-
+let p = document.createElement('p');
 
 // Set the inner HTML of the paragraph to be some bold text
-
+p.innerHTML = '<strong>Hello Circle Wrold!</strong>';
 
 // Select the element with class `container`, and append the paragraph to it
-
+document.querySelector('.container').appendChild(p);
 
 // Select `svg` element from the DOM
-
+let svg = document.querySelector('svg');
 
 // Set the width and height attributes using `setAttribute`
-
+svg.setAttribute('height', HEIGHT);
+svg.setAttribute('width', WIDTH);
 
 // Add a `rect` with the same width and height, and set the fill to `fill`
 // Note, you'll have to use `createElementNS` with our namespace
 // Also, set the pointerEvents to 'none'. Then, append this `rect` to the svg
-
+let rect = document.createElement(NS, 'rect');
+rect.setAttribute('x', 0);
+rect.setAttribute('y', 0);
+rect.setAttribute('width', WIDTH);
+rect.setAttribute('height', HEIGHT);
+rect.setAttribute('fill', FILL);
+rect.style.pointerEvents = 'none';
+svg.appendChild(rect);
 
 // Create a button with class "btn center-align" and append it to the element with class container.
 // You want to have it *inserted before* the div with class `svg-wrapper`
-
+let button = document.createElement('button');
+button.className = "btn center-align";
+button.textContent = "Clear!";
+let svgWrapper = document.querySelector('.svg-wraper');
+document.querySelector()
 
 // Function to render a circle in a parent element
 function renderCircle(coord, parent) {
     // Create the circle
-
+    let circle = document.createElement(NS, 'circle');
+    circle.setAttribute('cx', coord.x);
+    circle.setAttribute('cy', coord.y);
+    circle.setAttribute('r', 10); // do something cool here later
 
     // Define styles
-
+    circle.style.opacity = 0.3;
+    circle.style.pointerEvents = none;
+    circle.style.fill = `rgb(0, ${coord.x}, ${coord.y})`;
+    
     // Append
-
+    parent.appendChild(circle);
 }
 
 // Function to draw all circles in a parent
 function drawCircles(data, parent) {
-
+    data.forEach(function (d) {
+        renderCircle(d, parent);
+    });
 }
 
 // Function to clear circles from a parent
@@ -60,14 +80,17 @@ function clearCircles(parent) {
 // Assign event listener - on click:
 // - push a new data element into the state
 // - call the `update` function
-
+svg.addEventListener('click', function(event) {
+    state.circles.push({ x: event.offsetX, y: event.offsetY });
+    update();
+});
 
 // Update function: call `clearCircles`, `drawCircles`, and `updateText`
 function update() {
     // Clear container
-
+    let container = svg;
     // Update container contents
-
+    drawCircles(state.circles, container);
 }
 
 // Text function: change paragraph text to display the number of circles, and avg. color.
