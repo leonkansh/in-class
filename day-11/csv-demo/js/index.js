963ccc82944d8d2d69e6fea52e8247d6a343974f
Leon Kan <57278679+leonkansh@users.noreply.github.com>
2021-05-07 15:49:51 -0700

    followed demo and finished day-11/csv-demo

--- day-11/csv-demo/js/index.js
+++ day-11/csv-demo/js/index.js
@@ -1,7 +1,20 @@
 'use strict';
 
 // Use d3 to load the csv file using a *relative path*
-    // Iterate through the data and append a new <li> for each item
-    // The <li> should contain a link (<a>) to the website and 
-    // a <span> with the description about the website
+d3.csv("data/websites.csv").then(function(data) {
+    // console.log(data); // also work
+
+    data.map(function(d) {
+        // Iterate through the data and append a new <li> for each item
+        let li = $('<li>');
+        // The <li> should contain a link (<a>) to the website and 
+        let a = $(`<a href="${d.url}">${d.title}</a>`);
+        li.append(a);
+        // a <span> with the description about the website
+        li.append(`<span>: ${d.description}</span>`);
+        $('ul').append(li);
+    });
+    console.log(data); // also work
+
+})
     
