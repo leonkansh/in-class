59003c25a028cb9218f3776093aa0ec4c0df298f
Leon Kan <57278679+leonkansh@users.noreply.github.com>
2021-05-10 01:13:13 -0700

    failed to fully implement, checkout lecture 12.2

--- day-12/js/index.js
+++ day-12/js/index.js
@@ -4,11 +4,26 @@
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
-
+    constructor(
+        img = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cbc.ca%2Flife%2Fpets%2Fpuppies-get-pimples-plus-9-more-facts-that-might-surprise-first-time-dog-parents-1.5507364&psig=AOvVaw08upZ1rM7DWBo2uHYVhEk4&ust=1620714632190000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOi88dK-vvACFQAAAAAdAAAAABAD",
+        sound = "WOOF!"
+    ) {
+        this.img = img;
+        this.sound = sound;
+    }
     // Method for "speaking" using responsiveVoice
-
+    bark() {
+        responsiveVoice.speak(this.sound);
+    }
     // Render a Div that you can click on to bark
-
+    render() {
+        let puppyCard = $('<div>');
+        puppyCard.attr('title', `puppy that says ${this.sound}`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.css('background-image', `url(${this.img})`);
+        puppyCard.click(() => this.bark());
+        return puppyCard;
+    }
 }
 
 
@@ -52,23 +67,48 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
-
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        console.log(this);
+        this.puppyList.push({
+            url: img,
+            sound: sound
+        });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
-
+        let form = new PuppyForm(this.addPuppy.bind(this));
+        this.parentElement.append(form.render());
+        
         // Append puppy list element to parent (in a wrapper div)
-
+        let puppyWrapper = $('<div class="row"');
+        this.parentElement.append(puppyWrapper);
+        this.puppyList.map((puppyInfo) => {
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        })
+        
     }
 }
 
 // Create a new app with a single puppy
-
+let app = new PuppyApp(
+    $('#content'),
+    [{
+        url: "https://www.thesprucepets.com/thmb/EBp990AJt94XwAp7oOAzUtdg9Xg=/2121x1193/smart/filters:no_upscale()/golden-retriever-puppy-in-grass-923135452-5c887d4146e0fb00013365ba.jpg",
+        sound: "bark"
+    }]
+);
 
 // Render the app
+app.render();
