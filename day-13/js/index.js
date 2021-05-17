0dcceb1b6681fe491fc3f2e87cb1697393b52334
Leon Kan <57278679+leonkansh@users.noreply.github.com>
2021-05-17 12:13:05 -0700

    complete day-13

--- day-13/js/index.js
+++ day-13/js/index.js
@@ -2,13 +2,33 @@
 'use strict';
 
 // Data: a group of people
-const people = [
-    { name: "Jacob", interest: "Board Games" },
-    { name: "Vishank", interest: "Music" }
+const PEOPLELIST = [
+    { name: "Jacob", interest: "Board Games", location: "Seattle" },
+    { name: "Vishank", interest: "Music", location: "Seattle" }
 ];
 
 // Class for an individual person
-
+class Person extends React.Component {
+    render() {
+        return (
+            <div class='person'>
+                <p>Hello, my name is {this.props.name} and I am interested in {this.props.interest}.</p>
+                <p>I'm located in {this.props.location}!</p>
+            </div>
+        );
+    }
+}
 // Class to represent a group of people
+class People extends React.Component {
+    render() {
+        return (this.props.group.map((d) => {
+            return <Person key={d.name} name={d.name} name={d.name} interest={d.interest} location={d.location}/>
+        }))
+    }
+}
 
 // Render your component in the `main` section
+ReactDOM.render(
+    <People group={PEOPLELIST} />, // thing we want to render for
+    document.querySelector('main') // place we want to render at
+)
